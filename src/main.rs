4244ae6d9af91dffd43061831da5fc8eb52cbecd
jacoby <jacobyb0508@gmail.com>
2023-11-08 03:42:59 -0500

    Add lazy_static and regex dependencies, handle base64 image paths, and fix QR code detection.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use anyhow::Context;
 use base64::{engine::general_purpose, Engine as _};
-use image::{imageops::colorops::grayscale, io::Reader as ImageReader, DynamicImage};
+use image::{io::Reader as ImageReader, DynamicImage};
 use rqrr::PreparedImage;
+use std::path::PathBuf;
 use std::{env, io::Cursor};
 
 fn grab_image(url: &str) -> anyhow::Result<DynamicImage, anyhow::Error> {
@@ -18,26 +19,44 @@ fn grab_image(url: &str) -> anyhow::Result<DynamicImage, anyhow::Error> {
     Ok(image)
 }
 
-fn load_image(path: &str) -> anyhow::Result<DynamicImage, anyhow::Error> {
-    // Handling URLs
+fn decode_base64_path(path: &str) -> Result<image::DynamicImage, anyhow::Error> {
+    let base64_regex = regex::Regex::new(r#"^data:image/[^;]+;base64,([A-Za-z0-9+/]+={0,2})$"#).unwrap();
+    if let Some(captures) = base64_regex.captures(path) {
+        let base64_part = captures.get(1).unwrap().as_str();
+
+        let data = general_purpose::STANDARD.decode(base64_part.as_bytes())
+            .map_err(|_| anyhow::anyhow!("Failed to decode the base64 path"))?;
+        
+        let reader = ImageReader::new(std::io::Cursor::new(data));
+        let image = reader.with_guessed_format()?
+            .decode()
+            .context("Failed to decode the image")?;
+        
+        return Ok(image);
+    }
+
+    Err(anyhow::anyhow!("Failed to decode the base64 path"))
+}
+
+fn load_image(path: &str) -> Result<DynamicImage, anyhow::Error> {
+    let os_path = PathBuf::from(path);
+
+    if os_path.exists() {
+        return Ok(image::open(os_path)?);
+    }
+
     if path.contains("://") {
         return grab_image(path);
     }
 
-    // Handling base64
-    match general_purpose::STANDARD.decode(path.as_bytes()) {
-        Ok(data) => {
-            let reader = ImageReader::new(Cursor::new(data));
-
-            return Ok(reader.with_guessed_format()?.decode()?);
-        }
-        Err(_e) => {}
+    if path.starts_with("data:image/") {
+        return decode_base64_path(path);
     }
 
-    // Handling paths
-    Ok(DynamicImage::from(grayscale(&image::open(path)?)))
+    Ok(image::open(path)?)
 }
 
+// TODO: Use clap for argument parsing
 fn main() -> anyhow::Result<()> {
     let args: Vec<String> = env::args().collect();
     let image_path = &args[1];
@@ -58,7 +77,7 @@ fn main() -> anyhow::Result<()> {
     let mut prepared = PreparedImage::prepare(image.into_luma8());
     let grids = prepared.detect_grids();
 
-    assert!(grids.is_empty(), "No QR codes found in the image");
+    assert!(!grids.is_empty(), "No QR codes found in the image");
 
     let (_, content) = grids[0].decode()?;
     println!("{content}");
